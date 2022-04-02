6c3cccec8b589cd00ed322d15db11b3b50cada93
Brittany Fleury <brittany-fleury@uiowa.edu>
2022-04-02 12:31:52 -0500

    vtm_site initial commit
    created 3 html files necessary and modified app.py to include routes and functions for these pages.

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', pageTitle='Flask Server Home Page')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle='About Vertical Tank Maintenance')
+
+@app.route('/estimate')
+def estimate():
+    return render_template('estimate.html', pageTitle=' VTM Estimator')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
