8ff2bded458d768656a1eb4c43a2568868001e55
Brittany Fleury <brittany-fleury@uiowa.edu>
2022-04-02 18:44:41 -0500

    working on estimation page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from cmath import pi
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -12,20 +13,9 @@ def index():
 def about():
     return render_template('about.html', pageTitle='About Vertical Tank Maintenance')
 
-@app.route('/estimate', methods=['POST'])
+@app.route('/estimate')
 def estimate():
-    estimate= " "
-    if request.method=="POST" and 'tank_radius' in request.form and 'tank_height' in request.form:
-        radius=float(request.form.get('tank_radius'))
-        height=float(request.form.get('tank_height'))
-        area_tank_top= pi* radius^2
-        area_sides=2(pi(radius*height))
-        total_area= area_tank_top + area_sides
-        square_feet= total_area/144
-        material_cost= square_feet*25
-        labor_cost= square_feet*15
-        total_estimate= material_cost+ labor_cost
-    return render_template('estimate.html', pageTitle=' VTM Estimator', estimate=total_estimate)
+    return render_template('estimate.html', pageTitle=' VTM Estimator')
    
 
 
